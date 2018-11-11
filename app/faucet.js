ed1f21cffbd2e149683fbdee82fa256fce3eb7a7
Alexandru Sutii <alex@othera.io>
2018-11-11 19:21:37 +0100

    Made it load our contract

--- app/faucet.js
+++ app/faucet.js
@@ -1,50 +1,70 @@
-if (typeof web3 !== 'undefined') {
-    // Don't lose an existing provider, like Mist or Metamask
-    web3 = new Web3(web3.currentProvider);
-} else {
-    // set the provider you want from Web3.providers
-    web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-}
-web3.eth.getCoinbase(function(err, coinbase) {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log("Coinbase: " + coinbase);
+// Modern dapp browsers...
+if (window.ethereum) {
+    window.web3 = new Web3(ethereum);
+    try {
+        // Request account access if needed
+        ethereum.enable().then(function() {
+            goOn();
+        });
+    } catch (error) {
+        // User denied account access...
+        console.log("Cannot enable");
     }
-});
+}
+// Legacy dapp browsers...
+else if (window.web3) {
+    window.web3 = new Web3(web3.currentProvider);
+}
+// Non-dapp browsers...
+else {
+    console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+}
 
 // Your deployed address changes every time you deploy.
-const faucetAddress = "0xf8767ad850761e0682fbb4949a5c51f9c364311b"; // <-- Put your own
-const faucetContractFactory = web3.eth.contract(JSON.parse(faucetCompiled.contracts["Faucet.sol:Faucet"].abi));
-const faucetInstance = faucetContractFactory.at(faucetAddress);
+const faucetAddress = "0xbbb7ba2dbc1577ed33e1ae2a9c348c04c6bd4dcd"; // <-- Put your own
+var faucetContractFactory;
+var faucetInstance;
 
-// Query eth for balance
-web3.eth.getBalance(faucetAddress, function(err, balance) {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log("Contract balance: " + balance);
-    }
-});
+function goOn() {
+    web3.eth.getCoinbase(function(err, coinbase) {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log("Coinbase: " + coinbase);
+        }
+    });
 
-// Query the contract directly
-faucetInstance.getBalance.call(function(err, balance) {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log("Faucet balance: " + balance);
-    }
-});
+    faucetContractFactory = web3.eth.contract(JSON.parse(faucetCompiled.contracts["Faucet.sol:Faucet"].abi));
+    faucetInstance = faucetContractFactory.at(faucetAddress);
+
+    // Query eth for balance
+    web3.eth.getBalance(faucetAddress, function(err, balance) {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log("Contract balance: " + balance);
+        }
+    });
+
+    // Query the contract directly
+    faucetInstance.getBalance.call(function(err, balance) {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log("Faucet balance: " + balance);
+        }
+    });
+}
 
 function topUp() {
     web3.eth.getCoinbase(function(err, coinbase) {
         if (err) {
             console.error(err);
         } else {
-            web3.eth.sendTransaction({ 
-                from: coinbase, 
+            web3.eth.sendTransaction({
+                from: coinbase,
                 to: faucetAddress,
-                value: web3.toWei(1, "ether") 
+                value: web3.toWei(1, "ether")
             }, function(err, txn) {
                 if (err) {
                     console.error(err);
@@ -65,7 +85,7 @@ function sendWei() {
                 if (err) {
                     console.error(err);
                 } else {
-                    const targetAccount = accounts[1];      
+                    const targetAccount = accounts[0];
                     faucetInstance.sendWei(
                         targetAccount,
                         { from: coinbase },
